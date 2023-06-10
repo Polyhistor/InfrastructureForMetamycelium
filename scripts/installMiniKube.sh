144ae6346d19e6ef15f4ce2d1f74f73dbec15318
Pouya <pouya.ataei@ezyvet.com>
2023-06-10 14:45:12 +1200

    added two scripts, sourced them into main

--- scripts/installMiniKube.sh
+++ scripts/installMiniKube.sh
@@ -4,21 +4,10 @@
 green='\033[0;32m'
 bold=$(tput bold)
 
-# identifying the package manager
-declare -A osInfo;
-osInfo[/etc/redhat-release]="yum update && yum install"
-osInfo[/etc/arch-release]="pacman -S"
-osInfo[/etc/os-release]="pacman -S"
-osInfo[/etc/gentoo-release]="emerge -ask"
-osInfo[/etc/SuSE-release]="zypper update && zypper install"
-osInfo[/etc/debian_version]="apt update && apt-get install"
-osInfo[/etc/alpine-release]="apk update && apk add"
+# determining the right package manager
+source determineOsPackageManager.sh
 
-determineOsPackageManager(){
-    for f in ${!osInfo[@]}
-    do
-        if [[ -f $f ]];then
-            packageManagerInstallCommand=${osInfo[$f]}
-        fi
-    done
-}
+determineOsPackageManager
+
+# installing Minikube 
+source installMinikube.sh
