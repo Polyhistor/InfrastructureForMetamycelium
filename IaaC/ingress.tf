916731ca0261b0c6d7b06a764cf860edcb976f04
Pouya <pouya.ataei.7@gmail.com>
2023-06-14 20:32:34 +1200

    updated the release name

--- IaaC/ingress.tf
+++ IaaC/ingress.tf
@@ -1,6 +1,6 @@
 
 resource "helm_release" "mm_kubernetes_ingress" {
-  name       = "mm_ingress"
+  name       = "mm-ingress"
   repository = "https://polyhistor.github.io/helmChartsRepoForMetamycelium/kubernetes_ingress"
   chart      = "kubernetes-ingress"
   version    = "0.2.0"
