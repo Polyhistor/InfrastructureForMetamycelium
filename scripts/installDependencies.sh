62b0b101335eeefd7bb985065419fb3296846111
Pouya <pouya.ataei.7@gmail.com>
2023-06-18 18:37:06 +1200

    updated some scripts, added new ones, improved some others

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash 
 
 # global variables
-green='\033[0;32m'
-red='\033[0;31m'
-bold=$(tput bold)
-reset='\033[0m'
-
-export green
-export red  
-export bold
-export reset
+source globalVariables.sh
 
 # adding the scripts 
 source determineOsPackageManager.sh
--- scripts/isDockerRunning.sh
+++ scripts/isDockerRunning.sh
@@ -1,8 +1,9 @@
+#!/usr/bin/env bash 
+
 isDockerRunning() {
-     # Check if Docker is running
-    if ! docker info >/dev/null 2>&1; then
+    # Check if Docker is running
+    if ! systemctl is-active --quiet docker; then
         echo -e "${red}Docker is not running. Please make sure Docker is running.${reset}"
         exit 1
     fi
 }
- 
