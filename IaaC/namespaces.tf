b4bba7fa90e7c6db9669c82cb322186f9ef71c3f
Pouya <pouya.ataei.7@gmail.com>
2023-07-06 22:17:37 +1200

    added the Kiali Resources

--- IaaC/namespaces.tf
+++ IaaC/namespaces.tf
@@ -24,3 +24,17 @@ resource "helm_release" "prometheus" {
 
   depends_on = [helm_release.istiod]
 }
+
+resource "helm_release" "kiali" {
+  name       = "kiali"
+  chart      = "kiali-server"
+  repository = "https://kiali.org/helm-charts"
+  namespace  = kubernetes_namespace.istio_system.metadata[0].name
+
+  set {
+    name  = "external_services.prometheus.url"
+    value = "http://prometheus-server.${kubernetes_namespace.istio_system.metadata[0].name}.svc.cluster.local:9090"
+  }
+
+  depends_on = [helm_release.prometheus]
+}
