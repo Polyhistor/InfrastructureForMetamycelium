61b013f3f1838032d42de25485db9305ed519ef7
Pouya <pouya.ataei.7@gmail.com>
2023-07-06 22:15:22 +1200

    added prometheus to test Kiali dashboard

--- IaaC/namespaces.tf
+++ IaaC/namespaces.tf
@@ -14,3 +14,13 @@ resource "kubernetes_namespace" "istio_service_mesh" {
     }
   }
 }
+
+
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  chart      = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  namespace  = kubernetes_namespace.istio_system.metadata[0].name
+
+  depends_on = [helm_release.istiod]
+}
