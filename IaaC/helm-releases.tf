528b704afd337e2fc905006e88186fc8b6d55c39
Pouya Ataei <pouya-ataei@idexx.com>
2023-08-20 14:58:27 +1200

    updated the helm releases for prometheus

--- IaaC/helm-releases.tf
+++ IaaC/helm-releases.tf
@@ -95,6 +95,7 @@ resource "helm_release" "prometheus" {
   repository = "https://prometheus-community.github.io/helm-charts"
   namespace  = kubernetes_namespace.istio_system.metadata[0].name
 
+
   depends_on = [helm_release.istiod]
 }
 
